da700ed170a74227cc39b6f2fc71aed306a267a8
Sameer Tharakan <tharakan@login1.stampede2.tacc.utexas.edu>
2018-10-16 11:21:12 -0500

    adding testfile to see if hmlp interface works

---
+++ src/testcrf.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+
+//Simple testing class
+int main ()
+{
+	// Create image, probabilities, etc.
+	std::cout
+    << "Creating dummy problem .." << std:endl;
+
+	// Initialize CRF 
+	std::cout
+    << "Initializing CRF .." << std:endl;
+
+	// Run CRF
+	std::cout
+    << "Running CRF .. " << std:endl;
+
+	// Save output
+	std::cout
+    << "Successful CRF run, saving output" << std:endl;
+}
+
+
