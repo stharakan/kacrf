b1e1c9b2115f6783fbda89868878ca277b5323c3
Sameer Tharakan <tharakan@login1.stampede2.tacc.utexas.edu>
2018-10-16 11:34:28 -0500

    added stuff to actually try hmlp in testcrf, still need to add kernel things

--- src/testcrf.cpp
+++ src/testcrf.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
+#include <hmlp.h>
 
-
+using namespace std;
 
 //Simple testing class
-int main ()
+int main( )
 {
+	/** [Required] Problem size. */
+	size_t n = 5000;
+	/** Maximum leaf node size (not used in neighbor search). */
+	size_t m = 128;
+	/** [Required] Number of nearest neighbors. */
+	size_t k = 64;
+	/** Maximum off-diagonal rank (not used in neighbor search). */
+	size_t s = 128;
+	/** Approximation tolerance (not used in neighbor search). */
+	float stol = 1E-5;
+	/** The amount of direct evaluation (not used in neighbor search). */
+	float budget = 0.01;
+	/** Number of right-hand sides. */
+	size_t nrhs = 10;
+	/** Regularization for the system (K+lambda*I). */
+	float lambda = 1.0;	
+
+	// Initialize by calling hmlp api
+	hmlp_init( );
+	
 	// Create image, probabilities, etc.
-	std::cout
-    << "Creating dummy problem .." << std:endl;
+	std::cout << "Creating dummy problem .." << std::endl;
 
 	// Initialize CRF 
-	std::cout
-    << "Initializing CRF .." << std:endl;
+	std::cout << "Initializing CRF .." << std::endl;
 
 	// Run CRF
-	std::cout
-    << "Running CRF .. " << std:endl;
+	std::cout << "Running CRF .. " << std::endl;
 
 	// Save output
-	std::cout
-    << "Successful CRF run, saving output" << std:endl;
+	std::cout << "Successful CRF run, saving output" << std::endl;
+	
+	// Initialize by calling hmlp api
+	hmlp_finalize();
 }
 
 
