9d614b406889cb9b358a9f8df44bc37b1529cd3a
Sameer Tharakan <tharakan@login3.stampede2.tacc.utexas.edu>
2018-10-24 17:40:30 -0500

    updated tester, we now test gofmm interface + the pairwise messenger all works on s2

--- test/tester.cpp
+++ test/tester.cpp
@@ -1,14 +1,15 @@
-#include <iostream>
-//#include <hmlp.h>
-#include <gofmm/gofmm.hpp>
-#include <containers/KernelMatrix.hpp>
+//#include <gofmm.hpp>
+//#include <containers/SPDMatrix.hpp>
+//#include <containers/KernelMatrix.hpp>
 //#include <containers/VirtualMatrix.hpp>
-
+#include <gofmm_interface.hpp>
+#include <PairwiseMessage.hpp>
 
 using namespace std;
+using namespace kacrf;
 
 //Simple testing class
-int main( )
+int main( int argc, char *argv[] )
 {
 	/** [Required] Problem size. */
 	size_t n = 5000;
@@ -23,84 +24,61 @@ int main( )
 	/** The amount of direct evaluation (not used in neighbor search). */
 	float budget = 0.01;
 	/** Number of right-hand sides. */
-	size_t nrhs = 10;
+	size_t nrhs = 5;
 	/** Regularization for the system (K+lambda*I). */
 	float lambda = 1.0;	
 
 	// Initialize by calling hmlp api
-	hmlp_init( );
+  hmlp_init( &argc, &argv );
+
+
 
 	// other parameters needed
 	float h = 1.0;
-	size_t d = 6;
-	const bool ADAPTIVE = true;
-	const bool LEVELRESTRICTION = false;
-	//DistanceMetric metric = ANGLE_DISTANCE;
+	size_t d = 4;
+	size_t d2 = 10;
 
-	// Data
-	hmlp::Data<float> X( d, n ); 
-	X.randn();
+	// Feature sets, rand for now
+	hmlp::Data<float> X1( d, n ); 
+	X1.randn();
+	hmlp::Data<float> X2( d2, n ); 
+	X2.randn();
+	//hmlp::hmlp_printmatrix( (int) n, (int) n, X.data(), sizeof(float) );
+
+	// Initialize config
+	GoFMM_config config = ConfigureGoFMM();
+
+	// Initialize kernels
+	Kernel Kspa(X1, config);
+	Kernel Kapp(X2, config);
+	
+	// Compute message 
+	hmlp::Data<float> w1(n,nrhs ); w1.randn();
+	//auto u1 = K.Multiply(w1);
+	PairwiseMessenger pm(Kspa,Kapp,1.5, 10.0);
+	hData mm = pm.ComputeMessage(w1);
+
 
 	// Neighbor list
-	hmlp::Data<std::pair<float, std::size_t>> NN;
+	//hmlp::Data<std::pair<float, std::size_t>> NN;
 
-	// Set up kernel
-	kernel_s<float> kernel;
-	kernel.type = KS_GAUSSIAN;
-	kernel.scal = -0.5 / (h * h);
-
-	// Virtually initialize kernel matrix
-	hmlp::KernelMatrix<float> K1( n,n,d,kernel,X );
+	//// Virtually initialize kernel matrix
+	//hmlp::KernelMatrix<float> K2( X );
 
 
-	// Configure gofmm
-	hmlp::gofmm::Configuration<float> config( ANGLE_DISTANCE, n, m, k, s, stol, budget );
+	//// Configure gofmm
+	//hmlp::gofmm::Configuration<float> config2( ANGLE_DISTANCE, n, m, k, s, stol, budget );
 
-	// Rename tree splitting classes
-	using RKDTSPLITTER = hmlp::gofmm::randomsplit<hmlp::KernelMatrix<float>,2,float>;
-	using SPLITTER = hmlp::gofmm::centersplit<hmlp::KernelMatrix<float>,2,float>;
-
-	// initialize random tree
-	RKDTSPLITTER rkdtsplitter;
-	rkdtsplitter.Kptr = &K1;
-	rkdtsplitter.metric = ANGLE_DISTANCE;
-
-	// initialize ball tree
-	SPLITTER splitter;
-	splitter.Kptr = &K1;
-	splitter.metric = ANGLE_DISTANCE;
-		  
-	///** [Step#4] Perform the iterative neighbor search. */
-	//auto neighbors1 = hmlp::gofmm::FindNeighbors( K1, rkdtsplitter, config1 );
+	//hmlp::gofmm::randomsplit<KernelMatrix<float>, 2, float> rkdtsplitter2( K2 );
+  //hmlp::gofmm::centersplit<KernelMatrix<float>, 2, float> splitter2( K2 );
+	//auto neighbors2 = gofmm::FindNeighbors( K2, rkdtsplitter2, config2 );
 	//
-	///** [Step#5] Compress the matrix with an algebraic FMM. */
-	//auto* tree_ptr1 = gofmm::Compress( K1, neighbors1, splitter, rkdtsplitter, config1 );
-
-	auto* tree_ptr1 = hmlp::gofmm::Compress
-		<ADAPTIVE,LEVELRESTRICTION,SPLITTER,RKDTSPLITTER,float>
-		( &X,K1, NN, splitter, rkdtsplitter, config );
-	auto& tree1 = *tree_ptr1;
-
-	///** [Step#6] Compute an approximate MATVEC. */
-	
-	hmlp::Data<float> w1( nrhs, n ); w1.randn();
-	
-	auto u1 = hmlp::gofmm::Evaluate( tree1, w1 );
 	//
-	///** [Step#7] Factorization (HSS using ULV). */
-	//gofmm::Factorize( tree1, lambda ); 
+	//auto* tree_ptr2 = gofmm::Compress( K2, neighbors2, splitter2, rkdtsplitter2, config2 );
+  //auto& tree2 = *tree_ptr2;
 
-	//// Create image, probabilities, etc.
-	//std::cout << "Creating dummy problem .." << std::endl;
-
-	//// Initialize CRF 
-	//std::cout << "Initializing CRF .." << std::endl;
-
-	//// Run CRF
-	//std::cout << "Running CRF .. " << std::endl;
-
-	//// Save output
-	cout << "Successful CRF run, saving output" << std::endl;
+	//hmlp::Data<float> w1(n,nrhs ); w1.randn();
+	//
 	
 	// Finalize by calling hmlp api
 	hmlp_finalize();
