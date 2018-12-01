a2b874fa21b0fbbab0135bc42c51327bdd5039f5
Sameer Tharakan <sameer@ices.utexas.edu>
2018-12-01 09:59:09 -0600

    added s2 get/put functions

---
+++ ofiles/s2put.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Set defaults
+
+
+# Process command line
+
+
+
+# Make sbatch runfile
+
+
+
+# If test, leave, o.w. sbatch and rm
+
+
+
+usage="$(basename "$0") [-h] [-?] [-f ] -- bring file from s2:kacrf/ofiles to current dir
+
+where:
+	-h 	Show this help text
+	-? 	Show this help text
+	-f	specify the file (or just the last argument)"
+
+while getopts "h?:f:" opt; do
+	case "$opt" in
+		h|\?)
+			echo "$usage"
+			exit 0
+			;;
+		f) FILE=$OPTARG
+			;;
+	esac
+done
+shift $((OPTIND-1))
+
+# Check if a file is specified by option, otherwise it should be the last arg
+if [ -z "$FILE" ]
+then
+	if [[ -a "$@" ]]
+	then
+		FILE="$@"
+	else
+		echo "No file specified"
+		exit 0
+	fi
+fi
+
+if [ ! $TEST ]
+then
+	scp ./${FILE} tharakan@stampede2.tacc.utexas.edu:/home1/03158/tharakan/research/kacrf/ofiles/
+else
+	echo "transfering \"$FILE\" to this dir"
+fi
