14c38e8e6cdeaffba77be148df45774fae5c68c2
Sameer Tharakan <sameer@ices.utexas.edu>
2018-11-30 18:28:07 -0600

    added preliminary python functions to extract slices of brain and create dummy probabilities -- unsmoothed

---
+++ python/Images/brain.py
@@ -0,0 +1,192 @@
+import numpy as np
+import os
+import nibabel as nib
+from . import im_tools as imt
+
+
+class Brain:
+    def __init__(self,_bdir,_bname):
+        self.bdir = _bdir
+        self.bname = _bname
+    
+    def __str__(self):
+        return "Name: %s \nLoc: %s" % (self.bname, self.bdir)
+    
+    def ReadT2(self,slc=-1):
+        # Set up file
+        imname = self.bname + '_t2_normaff.nii.gz'
+        impath = os.path.join(self.bdir,imname)
+        
+        # load nifti
+        nii = nib.load(impath)
+        
+        # extract numpy array
+        if slc == -1:
+            return nii.get_data()
+        else:
+            bla = nii.get_data()
+            return bla[:,:,slc]
+    
+    def ReadT1(self,slc=-1):
+        # Set up file
+        imname = self.bname + '_t1_normaff.nii.gz'
+        impath = os.path.join(self.bdir,imname)
+        
+        # load nifti
+        nii = nib.load(impath)
+        
+        # extract numpy array
+        if slc == -1:
+            return nii.get_data()
+        else:
+            bla = nii.get_data()
+            return bla[:,:,slc]
+    
+    def ReadT1ce(self,slc=-1):
+        # Set up file
+        imname = self.bname + '_t1ce_normaff.nii.gz'
+        impath = os.path.join(self.bdir,imname)
+        
+        # load nifti
+        nii = nib.load(impath)
+        
+        # extract numpy array
+        if slc == -1:
+            return nii.get_data()
+        else:
+            bla = nii.get_data()
+            return bla[:,:,slc]
+    
+    def ReadFlair(self,slc=-1):
+        # Set up file
+        imname = self.bname + '_flair_normaff.nii.gz'
+        impath = os.path.join(self.bdir,imname)
+        
+        # load nifti
+        nii = nib.load(impath)
+        
+        # extract numpy array
+        if slc == -1:
+            return nii.get_data()
+        else:
+            bla = nii.get_data()
+            return bla[:,:,slc]
+    
+    def ReadSeg(self,slc=-1):
+        # Set up file
+        imname = self.bname + '_seg_aff.nii.gz'
+        impath = os.path.join(self.bdir,imname)
+        
+        # load nifti
+        nii = nib.load(impath)
+        
+        # extract numpy array
+        if slc == -1:
+            return nii.get_data()
+        else:
+            bla = nii.get_data()
+            return bla[:,:,slc]
+
+    def ReadAll(self, slc=-1):
+        seg = self.ReadSeg(slc) 
+        t1 = self.ReadT1(slc) 
+        t1c = self.ReadT1ce(slc) 
+        fl = self.ReadFlair(slc) 
+        t2 = self.ReadT2(slc) 
+
+        return t1, t1c, t2, fl, seg
+    
+    def SaveBrainToBinary(self):
+        # set up file name
+        imname = self.bname + '_allmods.bin'
+        impath = os.path.join(self.bdir,imname)
+        fid = open(impath,'bw')
+
+        # extract all modalities
+        t1,t1ce,t2,fl,seg = self.ReadAll(slc)
+
+        # extract all modalities
+        t1 = t1.reshape(t1.size,1,order='F')
+        t2 = t2.reshape(t2.size,1,order='F')
+        t1ce = t1ce.reshape(t1ce.size,1,order='F')
+        fl = fl.reshape(fl.size,1,order='F')
+
+        # reshape into large array 
+        full = np.concatenate([t1,t1ce,t2,fl],axis = 1) 
+
+        # write to binary, so that 4 int at one pixel are contiguous
+        full.tofile(fid)
+
+    def ReadBrainFromBinary():
+        # set up file name
+        imname = self.bname + '_allmods.bin'
+        impath = os.path.join(self.bdir,imname)
+
+        # read into array
+        am = np.fromfile(impath,dtype=np.float32)
+        am = am.reshape( (4,240,240,155), order = 'F')
+
+        return am
+
+    def SaveSliceProblem(self, slc=-1):
+        # set up file names
+        imname = self.bname + '_s' + str(slc) + '_allmods.bin'
+        segname = self.bname + '_s' + str(slc) + '_seg.bin'
+        probsname = self.bname + '_s' + str(slc) + '_probs.bin'
+    
+        # extract all modalities
+        t1,t1ce,t2,fl,seg = self.ReadAll(slc)
+
+        # extract probs
+        probs = imt.CreateProbsFromSeg2D(seg,sigma = 0.35)
+
+        # reshape each individual array
+        t1 = t1.reshape(-1,1,order='F')
+        t2 = t2.reshape(-1,1,order='F')
+        t1ce = t1ce.reshape(-1,1,order='F')
+        fl = fl.reshape(-1,1,order='F')
+        seg = seg.reshape(-1,1,order='F')
+
+        # reshape into large array 
+        full = np.concatenate([t1,t1ce,t2,fl],axis = 1) 
+
+        # write to binary, so that 4 int at one pixel are contiguous
+        impath = os.path.join(self.bdir,imname)
+        fid = open(impath,'bw')
+        full.tofile(fid)
+        fid.close()
+
+        # write seg to binary 
+        impath = os.path.join(self.bdir,segname)
+        fid2 = open(impath,'bw')
+        seg.tofile(fid2)
+        fid2.close()
+
+        # write probs to binary
+        impath = os.path.join(self.bdir,probsname)
+        fid3 = open(impath,'bw')
+        probs.tofile(fid3)
+        fid3.close()
+
+    def ReadSliceProblem(self,slc=-1):
+        # set up file names
+        imname = self.bname + '_s' + str(slc) + '_allmods.bin'
+        segname = self.bname + '_s' + str(slc) + '_seg.bin'
+        probsname = self.bname + '_s' + str(slc) + '_probs.bin'
+        
+        # write to binary, so that 4 int at one pixel are contiguous
+        impath = os.path.join(self.bdir,imname)
+        im = np.fromfile(impath,dtype=np.float32)
+        im = im.reshape( (4,240,240),order='F' ) 
+
+        # write seg to binary 
+        impath = os.path.join(self.bdir,segname)
+        seg = np.fromfile(impath,dtype=np.uint8)
+        seg = seg.reshape( (240, 240), order='F')
+
+        # write probs to binary
+        impath = os.path.join(self.bdir,probsname)
+        probs = np.fromfile(impath,dtype=np.float32)
+        probs = probs.reshape( (im.size,-1), order ='F')
+
+        return im, seg, probs
---
+++ python/Images/im_tools.py
@@ -0,0 +1,79 @@
+import numpy as np
+from sklearn.metrics import f1_score # dice score
+from sklearn.preprocessing import normalize # for resetting probabilities
+
+
+# get dice from unrounded probability matrix (has
+def BinaryDiceFromProbability(Q,seg,prob_cut=0.5):
+    # round probability 
+    qshape = Q.shape
+    yg = np.zeros( (qshape[0],1) )
+    yg[Q > prob_cut ] = 1.
+
+    # compute dice by calling other func here
+    dice = BinaryDice(yg,seg)
+
+    return dice
+
+# method to create image -- move to tools 
+def BinaryDice(ytr,yg,target = 1.):
+    ytr = ytr.flatten(order='F')
+    yg = yg.flatten(order='F')
+
+    return f1_score(ytr,yg,pos_label=target)
+
+# method to normalize probabilities -- can move it to a tools file 
+def NormalizeProbabilities(P):
+    P = normalize(P, axis=1, norm='l1')
+    P = ResetProbabilityZeros(P)
+    P = normalize(P, axis=1, norm='l1')
+    return P
+
+# method to reset probability zeros to values ok for log -- can move to tools 
+def ResetProbabilityZeros(P):
+    ep = np.finfo(np.float32).eps
+    P[ P < ep ] = ep
+    P[ P > (1-ep) ] = 1-ep
+    return P
+
+# method to create image -- move to tools 
+def CreateProbsFromSeg2D(seg,sigma = 0.35):
+
+    # make probabilities (bg and target)
+    probs_bg = np.random.randn( seg.shape[0],seg.shape[1] ) * sigma + 0.25
+    probs_tg = np.random.randn( seg.shape[0],seg.shape[1] ) * sigma + 0.75
+
+    # set final array
+    probs = np.where(seg != 0, probs_tg, probs_bg)
+    probs = ResetProbabilityZeros(probs)
+    probs = probs.reshape( (seg.size,1 ),order='F')
+    probs = np.concatenate( (probs,1-probs), axis=1 )
+
+    return probs
+
+
+# method to create image -- move to tools 
+def CreateImage(sz = 20, sigma = 0.3):
+    # get background
+    seg = np.zeros( (sz,sz) )
+
+    # index of nonzeros
+    start_idx = int(2*sz/5)
+    mid_idx = int(3*sz/5)
+    end_idx = int(4*sz/5)
+    target_size = mid_idx - start_idx
+    seg[ start_idx:mid_idx, mid_idx:end_idx ] = 1
+
+    # make probabilities and reshape
+    probs = np.random.randn( seg.shape[0],seg.shape[1] ) * sigma + 0.25
+    probs[ start_idx:mid_idx, mid_idx:end_idx] = np.random.randn( target_size,target_size ) * sigma + 0.75
+    probs = ResetProbabilityZeros(probs)
+    probs = probs.reshape( (int(sz*sz),1 ),order='F')
+    probs = np.concatenate( (probs,1-probs), axis=1 )
+
+    # make image -- N(1,1) -> bg; N(4,1) -> target
+    image = np.random.randn( seg.shape[0],seg.shape[1] ) + 1
+    image[ start_idx:mid_idx, mid_idx:end_idx ] = np.random.randn( target_size, target_size ) + 4
+
+    return image,probs,seg
+
