063e3902db3b79ca5ad973d34fbbf353e8957c79
Sameer Tharakan <tharakan@login3.stampede2.tacc.utexas.edu>
2018-12-07 12:27:06 -0600

    added function to run CRF over certain number of iterations, returning dice scores and final Q

--- src/optimizers.hpp
+++ src/optimizers.hpp
@@ -5,6 +5,8 @@
 #include <gofmm_interface.hpp>
 #include <utilities.hpp>
 #include <math.h> //exp, log
+#include <Brain.hpp>
+#include <PairwiseMessage.hpp>
 
 
 namespace kacrf
@@ -24,9 +26,11 @@ namespace kacrf
 			for (int ni = 0; ni < nn; ni++)
 			{
 				// switch classes
-				float temp = probs[ni];
-				probs[ni] = probs[ni + nn]; 
-				probs[ni + nn] = temp;
+				float t0 = probs(ni,0);
+				float t1 = probs(ni,1);
+
+				probs.setvalue(ni,0,t1); 
+				probs.setvalue(ni,1,t0); 
 			}
 
 		}
@@ -53,12 +57,30 @@ namespace kacrf
 			}
 		}
 	}; // end of potts model
+	
+	/** Update from dense crf without no unary */
+	void NoUnaryCRFUpdate(hData& Qmat, hData& pm, hData unary)
+	{
+		int nc = hDataSize(Qmat);
+
+		PottsMixture(pm);
+
+		#pragma omp parallel for
+		for (int i=0; i<nc; i++)
+		{
+			// Compute actual update
+			//float temp = log(unary[i]) - pm[i];
+
+			// Load into Qmat
+			Qmat[i] = exp(- pm[i]);
+		}
+
+	}; // end dense crf update
 
 	/** Update from dense crf */
 	void DenseCRFUpdate(hData& Qmat, hData& pm, hData unary)
 	{
 		int nc = hDataSize(Qmat);
-		hData preexp(Qmat.row(), Qmat.col());
 
 		PottsMixture(pm);
 
@@ -67,11 +89,10 @@ namespace kacrf
 		{
 			// Compute actual update
 			float temp = log(unary[i]) - pm[i];
-			preexp[i] = temp;	
-			Qmat[i] = temp;
+			//Qmat[i] = temp;
 
 			// Load into Qmat
-			//Qmat[i] = exp(temp);
+			Qmat[i] = exp(temp);
 		}
 		//std::cout << "update pre exp" << std::endl;
 		//preexp.Print();
@@ -81,6 +102,32 @@ namespace kacrf
 
 
 	}; // end dense crf update
+
+	// Run crf for a given set of kernels, length of dices determines iteration count
+	hData RunCRF( hData & dice_scores, Brain2D im, PairwiseMessenger pm,float targ=0.0)
+	{
+		int crf_iters = (int) dice_scores.size() - 1;
+		hData Q = im.Unary();
+		im.PrintDiceScore(dice_scores,targ); 
+	
+		for(int iter = 0; iter< crf_iters; iter++)
+		{
+			// Get pairwise message
+			hData m = pm.ComputeMessage(Q); 
+			
+			// Combine with unary for update
+			DenseCRFUpdate(Q, m,im.Unary()); 
+			
+			// Normalize update
+			NormalizeProbabilities(Q);
+			
+			// Evaluate accuracy and print 
+			im.PrintDiceScore(dice_scores,iter+1,Q,targ); 
+		}
+	
+		return Q;
+	
+	};
 
 }; // end namespace kacrf
 
