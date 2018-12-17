8b6626e8d98c455cd35091c66a71551500af0505
Sameer Tharakan <sameer@ices.utexas.edu>
2018-12-17 11:08:51 -0700

    changed make brain_slice_data to use new class method of bsp

--- python/make_brain_slice_data.py
+++ python/make_brain_slice_data.py
@@ -4,46 +4,71 @@ from Images import im_tools as imt
 import os
 import IPython
 from collections import Counter
+import matplotlib.pyplot as plt
 
 # initialize
-bname='Brats17_TCIA_621_1'
-bdir = os.path.join('/Users/sameer/Documents/data/trainingdata/',bname)
+bnames=['Brats17_TCIA_620_1','Brats17_TCIA_621_1','Brats17_TCIA_623_1','Brats17_TCIA_624_1','Brats17_TCIA_625_1']
+bnames=['Brats17_TCIA_623_1']
 odir = '/Users/sameer/Documents/research/kacrf/ofiles/'
+ddir = '/Users/sameer/Documents/research/kacrf/data/'
+ddir = None
 
-# create brain, read seg
-bb = br.Brain(bdir,bname)
-seg = bb.ReadSeg()
-
-# find indices of tumor
-i,j,k = np.nonzero(seg)
-
-# get 2 max locs
-cc = Counter(k)
-i1 = 1
-i2 = int( len(cc)/2 )
-slc1 = cc.most_common(i1)[i1-1][0]
-slc2 = cc.most_common(i2)[i2-1][0]
 noises = [0.05,0.10,0.15,0.2,0.25,0.3]
 noises = [0.05,0.10]
-noises = [0.4]
+noises = [1 ,2, 3 ]
 smt = 1 
 
-# save those to file
-for nz in noises:
-    #im, sg, pp = bb.SaveSliceProblem(sdir=odir,slc=slc1,noise = nz,smooth = smt)
-    bsp = bb.SaveSliceProblem(sdir=odir,slc=slc1,noise = nz,smooth = smt)
-    pp = bsp.probs
-    sg = bsp.seg
+for bname in bnames:
+    bdir = os.path.join('/Users/sameer/Documents/data/trainingdata/',bname)
+    # create brain, read seg
+    bb = br.Brain(bdir,bname)
+    seg = bb.ReadSeg()
+    
+    # find indices of tumor
+    i,j,k = np.nonzero(seg)
+    
+    # get 2 max locs
+    cc = Counter(k)
+    i1 = 1
+    i2 = int( len(cc)/2 )
+    slc1 = cc.most_common(i1)[i1-1][0]
+    slc2 = cc.most_common(i2)[i2-1][0]
+    
+    
+    # save those to file
+    for nz in noises:
+        bsp = br.BrainSliceProblem.CreateBSPFromSeg(bb.bdir,bb.bname,slc1,segsmooth=nz,smooth = smt )
+        pp = bsp.probs
+        sg = bsp.seg
+    
+        bsp.View(sdir = ddir)
+        #bsp.Save(sdir = odir)
+        #imt.VisImageFromFlat(pp[:,1])
+        #imt.VisImageFromFlat(pp[:,1] > 0.5)
+    
+        sg[sg != 0] = 1
+        d1 = imt.BinaryDiceFromProbability(pp,sg)
+    
+        bsp = br.BrainSliceProblem.CreateBSPFromSeg(bb.bdir,bb.bname,slc2,segsmooth=nz,smooth = smt )
+        pp = bsp.probs
+        sg = bsp.seg
+    
+        #bsp.View(sdir = ddir)
+        #bsp.Save(sdir = odir)
+        #imt.VisImageFromFlat(pp[:,1])
+        #imt.VisImageFromFlat(pp[:,1] > 0.5)
+    
+        sg[sg != 0] = 1
+        d2 = imt.BinaryDiceFromProbability(pp,sg)
+        th,tt,hh = bsp.ComputeMedianIntensityDistances()
+    
+        print("Brain: ", bname)
+        print("seg smooth: ",nz," psmooth: ",smt )
+        print(" d_tum (",slc1, " ): ", d1, " d_half(",slc2, " ): ", d2)
+        print(" th: " ,th ," tt: ", tt ," hh: ", hh)
 
-    imt.VisImageFromFlat(pp[:,1])
-    imt.VisImageFromFlat(pp[:,1] > 0.5)
 
-    sg[sg != 0] = 1
-
-    print(nz)
-    print(imt.BinaryDiceFromProbability(pp,sg))
-
-
+plt.show()
 
 
 #bb.SaveSliceProblem(sdir=odir,slc=slc2)
